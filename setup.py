87277e4a9df971277d44c2614b5195aae38e9f75
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2022-07-12 11:11:49 +0200

    [ADD] Add some meta-data

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name="hessianfree", packages=find_packages())
+setup(
+    name="hessianfree",
+    version="0.0",
+    description="PyTorch implementation of the Hessian-free optimizer",
+    author="Lukas Tatzel",
+    url="https://github.com/ltatzel/PyTorchHessianFree",
+    packages=find_packages(),
+)
