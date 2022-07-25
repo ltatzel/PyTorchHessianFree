5472ce18bf9d11925635f8a7c0b991f2841ea05c
Lukas Tatzel <lukas-nicola.tatzel@uni-tuebingen.de>
2022-07-25 12:58:28 +0200

    [REF] Upper limit for backpack and pytorch version

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ setup(
     author="Lukas Tatzel",
     url="https://github.com/ltatzel/PyTorchHessianFree",
     packages=find_packages(),
-    install_requires=["backpack-for-pytorch>=1.5.0", "torch>=1.11.0"],
+    install_requires=[
+        "backpack-for-pytorch>=1.5.0,<2.0.0",
+        "torch>=1.11.0,<2.0.0",
+    ],
     extras_require={
         "tests": [  # install with `pip install -e ".[tests]"`
             "pytest>=7.1.2",
         ],
         "examples": [  # install with `pip install -e ".[examples]"`
-            "pytest>=7.1.2",
             "deepobs @ " + DEEPOBS_LINK,
         ],
     },
